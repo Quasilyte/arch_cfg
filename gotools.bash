b4ef3e1872b6be47f14713d11c88c2bd1803a7b7
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-01-23 14:54:17 +0300

    Update gotools.bash

--- gotools.bash
+++ gotools.bash
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+# Generally useful Go tools.
 go get -v golang.org/x/perf/cmd/benchstat
+go get -v github.com/tzneal/bincmp
+go get -v github.com/golang/dep/cmd/dep
+
+# Tools for contributing to Go.
+# TODO.
