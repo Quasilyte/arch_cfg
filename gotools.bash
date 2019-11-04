262dd646f43ba288f3915cf70961fafb041217a6
Iskander (Alex) Sharipov <quasilyte@gmail.com>
2019-11-04 13:31:46 +0100

    gotools: add golint

--- gotools.bash
+++ gotools.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Generally useful Go tools.
+go get -v golang.org/x/lint/golint
 go get -v golang.org/x/perf/cmd/benchstat
 go get -v github.com/tzneal/bincmp
 go get -v github.com/golang/dep/cmd/dep
